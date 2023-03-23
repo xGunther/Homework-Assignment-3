04aea139dc5ef5121f9666de9d4080b4941648f6
xGunther <109365935+xGunther@users.noreply.github.com>
2023-03-23 22:37:19 +0100

    added a template for every movie page

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express();
 const port = 8058;
 
 app.use(express.static('public'));
-app.use(express.static('html'));
+app.use(express.static('index.html'));
+app.use(express.static('login.html'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/html/index.html');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
