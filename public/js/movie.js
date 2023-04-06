a07cefe21434657a2e81e1d727bc2a8b0085b27b
xGunther <109365935+xGunther@users.noreply.github.com>
2023-04-07 00:37:03 +0200

    cleaned up the code for movie pages

--- public/js/movie.js
+++ public/js/movie.js
@@ -2,91 +2,16 @@ let movieIdPlusOne;
 let i;
 let movieId = new URL(window.location).searchParams.get("id");
 
-
 function fetchMovieDescriptions() {
     fetch("movie-description-pages")
         .then(response => response.json())
         .then(data => {
-            if(movieId == "1"){
-                i = 0;
-                movieIdPlusOne = i + 1;
+            for (let j = 0; j < 20; j++) {
+                if (j == movieId) {
+                    i = movieId - 1;
+                    movieIdPlusOne = i + 1;
+                }
             }
-            else if(movieId == "2"){
-                i = 1;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "3"){
-                i = 2;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "4"){
-                i = 3;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "5"){
-                i = 4;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "6"){
-                i = 5;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "7"){
-                i = 6;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "8"){
-                i = 7;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "9"){
-                i = 8;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "10"){
-                i = 9;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "11"){
-                i = 10;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "12"){
-                i = 11;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "13"){
-                i = 12;
-                movieIdPlusOne = i + 1;
-            }    
-            else if(movieId == "14"){
-                i = 13;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "15"){
-                i = 14;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "16"){
-                i = 15;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "17"){
-                i = 16;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "18"){
-                i = 17;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "19"){
-                i = 18;
-                movieIdPlusOne = i + 1;
-            }
-            else if(movieId == "20"){
-                i = 19;
-                movieIdPlusOne = i + 1;
-            } 
             for (i; i < movieIdPlusOne; i++) {
                 const movieData = [
                     {term: "Director", definition: data[i].director},
